28ee62057fd4d0ea76280b62a33d8c7c4dd174f4
Nguyen Thanh Nam <namnguyen160504@gmail.com>
2025-10-15 14:27:28 +0700

    Fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: isProd ? 'export' : undefined,
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/namaewanam.github.io' : '',
-  assetPrefix: isProd ? '/namaewanam.github.io/' : '',
-  trailingSlash: true,
+  basePath: '/namaewanam.github.io',
 }
 
 module.exports = nextConfig
